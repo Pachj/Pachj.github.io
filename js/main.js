7fcd8586b898ff156c873854a36ebb1c4bcf3ebd
Pachj <Pachj@users.noreply.github.com>
2017-02-05 19:31:56 +0100

    some style changes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-/*$(document).ready(function () {
+$(document).ready(function () {
     $(window).resize(function () {
         if ($(this).width() <= 768) {
-            $("nav").removeClass("navbar-big-device").addClass("navbar-small-device");
-            $("h1").removeClass("h1-big-device").addClass("h1-small-device");
+
+            /*$("nav").removeClass("navbar-big-device").addClass("navbar-small-device");
+            $("h1").removeClass("h1-big-device").addClass("h1-small-device");*/            
         }
         else if ($(this).width() > 768) {
-            $( "nav" ).removeClass( "navbar-small-device" ).addClass( "navbar-big-device" );
-            $("h1").removeClass("h1-small-device").addClass("h1-big-device");
+            /*$( "nav" ).removeClass( "navbar-small-device" ).addClass( "navbar-big-device" );
+            $("h1").removeClass("h1-small-device").addClass("h1-big-device");*/
         }
     })
-});*/
+});
