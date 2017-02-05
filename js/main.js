e1f8c3ace069ae759ead5854ebba782748b1fe2c
Pachj <Pachj@users.noreply.github.com>
2017-02-05 19:20:50 +0100

    added querys but commentet the out
    some other fixes and other improvements

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*$(document).ready(function () {
+    $(window).resize(function () {
+        if ($(this).width() <= 768) {
+            $("nav").removeClass("navbar-big-device").addClass("navbar-small-device");
+            $("h1").removeClass("h1-big-device").addClass("h1-small-device");
+        }
+        else if ($(this).width() > 768) {
+            $( "nav" ).removeClass( "navbar-small-device" ).addClass( "navbar-big-device" );
+            $("h1").removeClass("h1-small-device").addClass("h1-big-device");
+        }
+    })
+});*/
