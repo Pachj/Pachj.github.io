87253d7d1e6e580c00928696597cc4e5f1d4fe3a
Pachj <Pachj@users.noreply.github.com>
2017-02-05 19:44:56 +0100

    font-size test

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
     $(window).resize(function () {
         if ($(this).width() <= 768) {
-
-            /*$("nav").removeClass("navbar-big-device").addClass("navbar-small-device");
-            $("h1").removeClass("h1-big-device").addClass("h1-small-device");*/            
+/*            $("nav").removeClass("navbar-big-device").addClass("navbar-small-device");
+            $("h1").removeClass("h1-big-device").addClass("h1-small-device");*/
+            $("h2").removeClass("h2-big-device").addClass("h2-small-device");            
         }
         else if ($(this).width() > 768) {
-            /*$( "nav" ).removeClass( "navbar-small-device" ).addClass( "navbar-big-device" );
+/*            $( "nav" ).removeClass( "navbar-small-device" ).addClass( "navbar-big-device" );
             $("h1").removeClass("h1-small-device").addClass("h1-big-device");*/
+            $("h2").removeClass("h2-small-device").addClass("h2-big-device");
         }
     })
 });
