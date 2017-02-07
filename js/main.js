4da5f3958b1b75e1230ac62c52a186a4e1d79c3f
Pachj <Pachj@users.noreply.github.com>
2017-02-07 10:55:56 +0100

    many little changes added socialmedia links

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@ $(document).ready(function () {
         if ($(this).width() <= 768) {
 /*            $("nav").removeClass("navbar-big-device").addClass("navbar-small-device");
             $("h1").removeClass("h1-big-device").addClass("h1-small-device");*/
-            $("h2").removeClass("h2-big-device").addClass("h2-small-device");            
+            $("h2").removeClass("h2-big-device").addClass("h2-small-device");  
+            /*$("#showcase-image-2").prepend("<div class='row'>");
+            $("#showcase-text-2").append("</div>");*/                     
         }
         else if ($(this).width() > 768) {
 /*            $( "nav" ).removeClass( "navbar-small-device" ).addClass( "navbar-big-device" );
             $("h1").removeClass("h1-small-device").addClass("h1-big-device");*/
-            $("h2").removeClass("h2-small-device").addClass("h2-big-device");
+            $("h2").removeClass("h2-small-device").addClass("h2-big-device");           
         }
     })
 });
