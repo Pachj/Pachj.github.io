5036f89005b981eca89c457b3c94f3ec0b136a1a
Pachj <Pachj@users.noreply.github.com>
2017-02-07 11:32:30 +0100

    responsive font-size for <p>

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@ $(document).ready(function () {
 /*            $("nav").removeClass("navbar-big-device").addClass("navbar-small-device");
             $("h1").removeClass("h1-big-device").addClass("h1-small-device");*/
             $("h2").removeClass("h2-big-device").addClass("h2-small-device");  
-            /*$("#showcase-image-2").prepend("<div class='row'>");
-            $("#showcase-text-2").append("</div>");*/                     
+            $("p").removeClass("p-big-device").addClass("p-small-device");                   
         }
         else if ($(this).width() > 768) {
 /*            $( "nav" ).removeClass( "navbar-small-device" ).addClass( "navbar-big-device" );
             $("h1").removeClass("h1-small-device").addClass("h1-big-device");*/
-            $("h2").removeClass("h2-small-device").addClass("h2-big-device");           
+            $("h2").removeClass("h2-small-device").addClass("h2-big-device"); 
+            $("p").removeClass("p-small-device").addClass("p-big-device");           
         }
     })
 });
